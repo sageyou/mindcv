0a06fed1f08a42865a8c5c9edc9318f0b08b44ee
wuzhf9 <wuzhf9@mail2.sysu.edu.cn>
2023-04-15 15:15:44 +0800

    update beit

--- mindcv/models/beit.py
+++ mindcv/models/beit.py
@@ -1,8 +1,7 @@
-import os
 import math
 import numpy as np
 from collections import OrderedDict
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 from functools import partial
 
 import mindspore as ms
@@ -160,7 +159,7 @@ class Mlp(nn.Cell):
         hidden_features: Optional[int] = None,
         out_features: Optional[int] = None,
         act_layer: nn.Cell = nn.GELU,
-        drop: float = 0.0
+        drop: float = 0.
     ):
         super(Mlp, self).__init__()
         out_features = out_features or in_features
@@ -168,7 +167,7 @@ class Mlp(nn.Cell):
         self.fc1 = nn.Dense(in_channels=in_features, out_channels=hidden_features, has_bias=True)
         self.act = act_layer()
         self.fc2 = nn.Dense(in_channels=hidden_features, out_channels=out_features, has_bias=True)
-        self.drop = nn.Dropout(keep_prob=1.0 - drop)
+        self.drop = nn.Dropout(1 - drop)
 
     def construct(self, x):
         x = self.fc1(x)
@@ -188,8 +187,9 @@ class Attention(nn.Cell):
         qk_scale: Optional[float] = None,
         attn_drop: float = 0.0,
         proj_drop: float = 0.0,
-        window_size: Optional[Tuple] = None,
         attn_head_dim: Optional[int] = None,
+        rel_pos_bias: Union[nn.Cell, bool] = False,
+        window_size: Optional[Tuple] = None
     ):
         super(Attention, self).__init__()
         self.num_heads = num_heads
@@ -208,7 +208,9 @@ class Attention(nn.Cell):
         else:
             self.qkv = nn.Dense(dim, all_head_dim * 3, has_bias=False)
 
-        if window_size is not None:
+        if isinstance(rel_pos_bias, RelativePositionBias):
+            self.relative_position_bias = rel_pos_bias
+        elif rel_pos_bias:
             self.relative_position_bias = RelativePositionBias(window_size, num_heads)
         else:
             self.relative_position_bias = None
@@ -225,7 +227,7 @@ class Attention(nn.Cell):
         self.q_matmul_k = ops.BatchMatMul(transpose_b=True)
         self.softmax = nn.Softmax(axis=-1)
 
-    def construct(self, x, rel_pos_bias=None):
+    def construct(self, x):
         b, n, c = x.shape
         qkv = self.qkv(x)
         qkv = self.reshape(qkv, (b, n, 3, self.num_heads, c // self.num_heads))
@@ -235,11 +237,9 @@ class Attention(nn.Cell):
         attn = self.q_matmul_k(q, k)
         attn = self.mul(attn, self.scale)
 
-        if rel_pos_bias is not None:
-            attn = attn + rel_pos_bias
-        elif self.relative_position_bias is not None:
+        if self.relative_position_bias is not None:
             rel_pos_bias = self.relative_position_bias()
-            attn = attn + rel_pos_bias
+            attn  = attn + rel_pos_bias
 
         attn = self.softmax(attn)
         attn = self.attn_drop(attn)
@@ -253,116 +253,119 @@ class Attention(nn.Cell):
         return out
 
 
+class LayerScale(nn.Cell):
+    def __init__(
+        self,
+        dim: int,
+        init_values: float = 1e-5
+    ):
+        super(LayerScale, self).__init__()
+        self.gamma = Parameter(initializer(init_values, dim))
+    
+    def construct(self, x):
+        return self.gamma * x
+    
+
 class Block(nn.Cell):
     def __init__(
         self,
         dim: int,
-        num_heads: int,
-        mlp_ratio: float = 4.0,
+        num_heads: int = 8,
         qkv_bias: bool = False,
         qk_scale: Optional[float] = None,
-        drop: float = 0.0,
-        attn_drop: float = 0.0,
-        drop_path: float = 0.0,
+        attn_drop: float = 0.,
+        proj_drop: float = 0.,
+        attn_head_dim: Optional[int] = None,
+        mlp_ratio: float = 4.,
+        drop_path: float = 0.,
         init_values: Optional[float] = None,
         act_layer: nn.Cell = nn.GELU,
         norm_layer: nn.Cell = nn.LayerNorm,
-        window_size: Optional[Tuple] = None,
-        attn_head_dim: Optional[int] = int
+        rel_pos_bias: Union[nn.Cell, bool] = False,
+        window_size: Optional[Tuple] = None
     ):
         super(Block, self).__init__()
         self.norm1 = norm_layer((dim,))
         self.attn = Attention(
-            dim=dim, num_heads= num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            attn_drop=attn_drop, proj_drop=drop, window_size=window_size, attn_head_dim=attn_head_dim
+            dim=dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=proj_drop, attn_head_dim=attn_head_dim,
+            rel_pos_bias=rel_pos_bias, window_size=window_size
         )
-        self.drop_path = DropPath(drop_path) if drop_path > 0 else nn.Identity()
-        self.norm2 = norm_layer((dim,))
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(
-            in_features=dim, hidden_features=mlp_hidden_dim,
-            act_layer=act_layer, drop=drop
-        )
-        if init_values is not None and init_values > 0:
-            self.gamm_1 = Parameter(initializer(init_values, dim))
-            self.gamm_2 = Parameter(initializer(init_values, dim))
-        else:
-            self.gamm_1, self.gamm2 = None, None
+        self.ls1 = LayerScale(dim=dim, init_values=init_values) if init_values else nn.Identity()
+        self.drop_path1 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
-    def construct(self, x, rel_pos_bias=None):
-        if self.gamm_1 is None:
-            x = x + self.drop_path(self.attn(self.norm1(x), rel_pos_bias))
-            x = x + self.drop_path(self.mlp(self.norm2(x)))
-        else:
-            x = x + self.drop_path(self.gamm_1 * self.attn(self.norm1(x), rel_pos_bias))
-            x = x + self.drop_path(self.gamm_2 * self.mlp(self.norm2(x)))
+        self.norm2 = norm_layer((dim,))
+        self.mlp = Mlp(
+            in_features=dim, hidden_features=int(dim * mlp_ratio),
+            act_layer=act_layer, drop=proj_drop
+        )
+        self.ls2 = LayerScale(dim=dim, init_values=init_values) if init_values else nn.Identity()
+        self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def construct(self, x):
+        x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
+        x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
         return x
 
 
-class VisionTransformerForMaskedImageModeling(nn.Cell):
+class VisionTransformerEncoder(nn.Cell):
     def __init__(
         self,
         img_size: int = 224,
         patch_size: int = 16,
         in_chans: int = 3,
-        vocab_size: int = 8192,
         embed_dim: int = 768,
         depth: int = 12,
         num_heads: int = 12,
-        mlp_ratio: int = 4,
+        attn_head_dim: Optional[int] = None,
+        mlp_ratio: float = 4.,
         qkv_bias: bool = True,
         qk_scale: Optional[float] = None,
-        drop_rate: float = 0.0,
-        attn_drop_rate: float = 0.0,
-        drop_path_rate: float = 0.0,
-        norm_layer: Optional[nn.Cell] = None,
+        pos_drop_rate: float = 0.,
+        proj_drop_rate: float = 0.,
+        attn_drop_rate: float = 0.,
+        drop_path_rate: float = 0.,
         init_values: Optional[float] = 0.1,
-        attn_head_dim: Optional[int] = None,
+        act_layer: nn.Cell = nn.GELU,
+        norm_layer: nn.Cell = nn.LayerNorm,
         use_abs_pos_emb: bool = False,
         use_rel_pos_bias: bool = False,
         use_shared_rel_pos_bias: bool = True,
-        init_std: float = 0.02,
         **kwargs
     ):
-        super(VisionTransformerForMaskedImageModeling, self).__init__()
-        self.num_features = self.embed_dim = embed_dim
-
+        super(VisionTransformerEncoder, self).__init__()
+        self.embed_dim = embed_dim
         self.patch_embed = PatchEmbed(image_size=img_size, patch_size=patch_size,
                                       in_chans=in_chans, embed_dim=embed_dim)
-        num_patches = self.patch_embed.num_patches
+        self.num_patches = self.patch_embed.num_patches
 
         self.cls_token = Parameter(initializer('truncatedNormal', (1, 1, embed_dim)))
-        self.mask_token = Parameter(initializer('truncatedNormal', (1, 1, embed_dim)))
-        if use_abs_pos_emb:
-            self.pos_emb = Parameter(initializer('truncatedNormal', (1, num_patches + 1, embed_dim)))
-        else:
-            self.pos_emb = None
-        self.pos_drop = nn.Dropout(1 - drop_rate)
+        
+        self.pos_emb = Parameter(initializer('truncatedNormal', (1, self.num_patches + 1, embed_dim))) if use_abs_pos_emb else None
+        self.pos_drop = nn.Dropout(1 - pos_drop_rate)
 
         if use_shared_rel_pos_bias:
-            self.rel_pos_bias = RelativePositionBias(window_size=self.patch_embed.patches_resolution,
-                                                     num_heads=num_heads)
+            rel_pos_bias = RelativePositionBias(window_size=self.patch_embed.patches_resolution,
+                                                num_heads=num_heads)
+        elif use_rel_pos_bias:
+            rel_pos_bias = True
         else:
-            self.rel_pos_bias = None
+            rel_pos_bias = False
 
         dpr = [x.item() for x in np.linspace(0, drop_path_rate, depth)]
         self.blocks = nn.CellList([
             Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias,
-                qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i],
-                norm_layer=norm_layer, init_values=init_values,
-                window_size=self.patch_embed.patches_resolution if use_rel_pos_bias else None,
-                attn_head_dim=attn_head_dim
+                dim=embed_dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                attn_drop=attn_drop_rate, proj_drop=proj_drop_rate, attn_head_dim=attn_head_dim,
+                mlp_ratio=mlp_ratio, drop_path=dpr[i], init_values=init_values,
+                act_layer=act_layer, norm_layer=norm_layer, rel_pos_bias=rel_pos_bias,
+                window_size=self.patch_embed.patches_resolution 
             ) for i in range(depth)
         ])
-        self.norm = norm_layer((embed_dim,))
 
-        self.init_std = init_std
-        self.lm_head = nn.Dense(embed_dim, vocab_size, weight_init='truncatedNormal')
-
-        self._init_weights()
-        self._fix_init_weights()
-
+    def get_num_layeres(self):
+        return len(self.blocks)
+    
     def _init_weights(self):
         for _, cell in self.cells_and_names():
             if isinstance(cell, nn.Dense):
@@ -398,39 +401,182 @@ class VisionTransformerForMaskedImageModeling(nn.Cell):
                 ops.div(block.mlp.fc2.weight, math.sqrt(2.0 * (i + 1)))
             )
 
-    def no_weight_decay(self):
-        return {'pos_emb', 'cls_token'}
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        bsz = x.shape[0]
 
-    def get_num_layers(self):
-        return len(self.blocks)
+        cls_token = ops.broadcast_to(self.cls_token, (bsz, -1, -1))
+        x = ops.concat((cls_token, x), axis=1)
+        
+        if self.pos_emb is not None:
+            x = x + self.pos_emb
+        x = self.pos_drop
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        return x
+
+    def construct(self, x):
+        return self.forward_features(x)
+
+
+class BEiTForPretrain(VisionTransformerEncoder):
+    def __init__(
+        self,
+        img_size: int = 224,
+        patch_size: int = 16,
+        in_chans: int = 3,
+        embed_dim: int = 768,
+        depth: int = 12,
+        num_heads: int = 12,
+        attn_head_dim: Optional[int] = None,
+        mlp_ratio: float = 4.,
+        qkv_bias: bool = True,
+        qk_scale: Optional[float] = None,
+        pos_drop_rate: float = 0.,
+        proj_drop_rate: float = 0.,
+        attn_drop_rate: float = 0.,
+        drop_path_rate: float = 0.,
+        init_values: Optional[float] = 0.1,
+        act_layer: nn.Cell = nn.GELU,
+        norm_layer: nn.Cell = nn.LayerNorm,
+        use_abs_pos_emb: bool = False,
+        use_rel_pos_bias: bool = False,
+        use_shared_rel_pos_bias: bool = True,
+        vocab_size: int = 8192,
+        **kwargs
+    ):
+        super(BEiTForPretrain, self).__init__(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+            depth=depth,
+            num_heads=num_heads,
+            attn_head_dim=attn_head_dim,
+            mlp_ratio=mlp_ratio,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            pos_drop_rate=pos_drop_rate,
+            proj_drop_rate=proj_drop_rate,
+            attn_drop_rate=attn_drop_rate,
+            drop_path_rate=drop_path_rate,
+            init_values=init_values,
+            act_layer=act_layer,
+            norm_layer=norm_layer,
+            use_abs_pos_emb=use_abs_pos_emb,
+            use_rel_pos_bias=use_rel_pos_bias,
+            use_shared_rel_pos_bias=use_shared_rel_pos_bias,
+            **kwargs
+        )
+        self.mask_token = Parameter(initializer('truncatedNormal', (1, 1, embed_dim)))
+        self.head = nn.Dense(embed_dim, vocab_size, weight_init='truncatedNormal')
+        self.norm = norm_layer((embed_dim, ))
+
+        self._init_weights()
+        self._fix_init_weights() 
 
     def forward_features(self, x, bool_masked_pos):
         x = self.patch_embed(x)
         bsz, seq_len, _ = x.shape
 
-        cls_tokens = ops.broadcast_to(self.cls_token, (bsz, -1, -1))
         mask_tokens = ops.broadcast_to(self.mask_token, (bsz, seq_len, -1))
-
         w = ops.expand_dims(bool_masked_pos, axis=-1).astype(mask_tokens.dtype)
         x = x * (1 - w) + mask_tokens * w
 
+        cls_tokens = ops.broadcast_to(self.cls_token, (bsz, -1, -1))
         x = ops.concat((cls_tokens, x), axis=1)
+
         if self.pos_emb is not None:
             x = x + self.pos_emb
         x = self.pos_drop(x)
 
-        rel_pos_bias = self.rel_pos_bias() if self.rel_pos_bias is not None else None
         for blk in self.blocks:
-            x = blk(x, rel_pos_bias)
+            x = blk(x)
 
         x = self.norm(x)
         return x
 
     def construct(self, x, bool_masked_pos):
         x = self.forward_features(x, bool_masked_pos)
+        x = self.norm(x)
         x = x[:, 1:]
-        return self.lm_head(x)
+        x = self.head(x)
+        return x
 
+
+class BEiTForFinetune(VisionTransformerEncoder):
+    def __init__(
+        self,
+        img_size: int = 224,
+        patch_size: int = 16,
+        in_chans: int = 3,
+        embed_dim: int = 768,
+        depth: int = 12,
+        num_heads: int = 12,
+        attn_head_dim: Optional[int] = None,
+        mlp_ratio: float = 4.,
+        qkv_bias: bool = True,
+        qk_scale: Optional[float] = None,
+        pos_drop_rate: float = 0.,
+        proj_drop_rate: float = 0.,
+        attn_drop_rate: float = 0.,
+        drop_path_rate: float = 0.,
+        init_values: Optional[float] = 0.1,
+        act_layer: nn.Cell = nn.GELU,
+        norm_layer: nn.Cell = nn.LayerNorm,
+        use_abs_pos_emb: bool = False,
+        use_rel_pos_bias: bool = False,
+        use_shared_rel_pos_bias: bool = True,
+        num_classes: int = 1000,
+        use_mean_pooling: bool = True,
+        init_scale: float = 0.001,
+        **kwargs
+    ):
+        super(BEiTForFinetune, self).__init__(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+            depth=depth,
+            num_heads=num_heads,
+            attn_head_dim=attn_head_dim,
+            mlp_ratio=mlp_ratio,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            pos_drop_rate=pos_drop_rate,
+            proj_drop_rate=proj_drop_rate,
+            attn_drop_rate=attn_drop_rate,
+            drop_path_rate=drop_path_rate,
+            init_values=init_values,
+            act_layer=act_layer,
+            norm_layer=norm_layer,
+            use_abs_pos_emb=use_abs_pos_emb,
+            use_rel_pos_bias=use_rel_pos_bias,
+            use_shared_rel_pos_bias=use_shared_rel_pos_bias,
+            **kwargs
+        )
+        self.use_mean_pooling = use_mean_pooling
+        self.head = nn.Dense(embed_dim, num_classes, weight_init='TruncatedNormal')
+        self.norm = norm_layer((embed_dim,))
+
+        self._init_weights()
+        self._fix_init_weights()
+        
+        self.head.weight.set_data(ops.mul(self.head.weight, init_scale))
+        self.head.bias.set_data(ops.mul(self.head_bias, init_scale))
+
+    def construct(self, x):
+        x = self.forward_features(x)
+        if self.use_mean_pooling:
+            x = x[:, 1:].mean(axis=1)
+            x = self.norm(x)
+        else:
+            x = self.norm(x)
+            x = x[:, 0]
+        x = self.head(x)
+        return x
 
 @register_model
 def dall_e(pretrained=False, freeze=True, **kwargs):
@@ -447,7 +593,7 @@ def dall_e(pretrained=False, freeze=True, **kwargs):
 
 @register_model
 def beit_b_16_224_8k_vocab(pretrained=False, **kwargs):
-    model = VisionTransformerForMaskedImageModeling(
+    model = BEiTForPretrain(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, epsilon=1e-6), vocab_size=8192, **kwargs
     )
@@ -458,7 +604,7 @@ def beit_b_16_224_8k_vocab(pretrained=False, **kwargs):
 
 @register_model
 def beit_l_16_224_8k_vocab(pretrained=False, **kwargs):
-    model = VisionTransformerForMaskedImageModeling(
+    model = BEiTForPretrain(
         patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, epsilon=1e-6), vocab_size=8192, **kwargs
     )
